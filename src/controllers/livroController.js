dddf791c85d6abe2c6adb6f0d4ad7a7d7e162843
Dirceusljr <dirceusljr@gmail.com>
2024-06-06 15:39:50 -0300

    refatorando listarLivros e criação do listarLivro e atualizarLivro

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -3,8 +3,22 @@ import livro from "../models/Livro.js";
 class LivroController {
 
     static async ListarLivros(req, res) {
-        const listaLivros = await livro.find({})
-        res.status(200).json(listaLivros)
+        try {
+            const listaLivros = await livro.find({})
+            res.status(200).json(listaLivros)
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - falha na requisição` })
+        }
+    }
+
+    static async ListarLivro(req, res) {
+        try {
+            const id = req.params.id;
+            const livroEncontrado = await livro.findById(id)
+            res.status(200).json(livroEncontrado)
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - falha na requisição do livro` })
+        }
     }
 
     static async CadastrarLivro(req, res) {
@@ -16,6 +30,16 @@ class LivroController {
             })
         } catch (error) {
             res.status(500).json({ message: `${error.message} - falha ao cadastrar livro` })
+        }
+    }
+
+    static async atualizarLivro(req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndUpdate(id, req.body)
+            res.status(200).json({ message: 'Livro atualizado com sucesso!' })
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - falha na atualização do livro` })
         }
     }
 }
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -4,5 +4,8 @@ import LivroController from "../controllers/livroController.js";
 const router = express.Router();
 
 router.get('/livros', LivroController.ListarLivros)
+router.get('/livros/:id', LivroController.ListarLivro)
+router.post('/livros', LivroController.CadastrarLivro)
+router.put('/livros/:id', LivroController.atualizarLivro)
 
 export default router;
