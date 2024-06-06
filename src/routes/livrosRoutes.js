d0dc515882164c92321acf793052c8be98619109
Dirceusljr <dirceusljr@gmail.com>
2024-06-06 14:49:00 -0300

    criação do controller e do routes

---
+++ src/routes/livrosRoutes.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import LivroController from "../controllers/livroController.js";
+
+const router = express.Router();
+
+router.get('/livros', LivroController.ListarLivros)
+
