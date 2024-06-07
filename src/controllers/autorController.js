c6e66c94a75e541297b829ffbebaf4b533712dc5
Dirceusljr <dirceusljr@gmail.com>
2024-06-07 13:43:53 -0300

    tratando dos erros no ListarAutor

--- src/controllers/autorController.js
+++ src/controllers/autorController.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import { autor } from '../models/Autor.js'
 
 class AutorController {
@@ -14,9 +15,17 @@ class AutorController {
     try {
       const id = req.params.id
       const autorEncontrado = await autor.findById(id)
-      res.status(200).json(autorEncontrado)
+      if (autorEncontrado !== null) {
+        res.status(200).json(autorEncontrado)
+      } else {
+        res.status(404).json({ message: "Id do Autor não localizado." })
+      }
     } catch (error) {
-      res.status(500).json({ message: `${error.message} - falha na requisição do autor` })
+      if (error instanceof mongoose.Error.CastError ) {
+        res.status(400).json({ message: "Um ou mais dados informados são inválidos." })
+      } else {
+        res.status(500).json({message: "Erro interno de servidor."})
+      }
     }
   }
 
