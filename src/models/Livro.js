4788ca6665e8212a9ec3b39cb29ae5258b1eac8c
Dirceusljr <dirceusljr@gmail.com>
2024-06-09 16:30:10 -0300

    tratando de validação customizada com o moongose

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -16,8 +16,14 @@ const LivroSchema = new mongoose.Schema({
   preco: { type: Number },
   paginas: { 
     type: Number,
-    min: [10, 'O número de páginas deve ser entre 10 e 5000. Valor fornecido: {VALUE}.'],
-    max: [5000, 'O número de páginas deve ser entre 10 e 5000. Valor fornecido: {VALUE}.'],
+    validate: {
+      validator: (valor) => {
+        return valor >= 10 && valor <= 5000
+      },
+      message: 'O número de páginas deve ser entre 10 e 5000. Valor fornecido: {VALUE}.'
+    }
+    // min: [10, 'O número de páginas deve ser entre 10 e 5000. Valor fornecido: {VALUE}.'],
+    // max: [5000, 'O número de páginas deve ser entre 10 e 5000. Valor fornecido: {VALUE}.'],
   },
   autor: {
     type: mongoose.Schema.Types.ObjectId,
