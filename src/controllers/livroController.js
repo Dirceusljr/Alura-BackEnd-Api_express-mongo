f530e13582af7e527c29a942212778a17b7bc2d3
Dirceusljr <dirceusljr@gmail.com>
2024-06-10 14:20:29 -0300

    aplicando ordenação

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -5,13 +5,17 @@ import { Autor, Livro } from '../models/index.js'
 class LivroController {
   static async ListarLivros(req, res, next) {
     try {
-      let { limite = 5, pagina = 1 } = req.query;
+      let { limite = 5, pagina = 1, ordenacao="_id:1" } = req.query;
+
+      let [campoOrdenacao, ordem] = ordenacao.split(":");
 
       limite = parseInt(limite)
       pagina = parseInt(pagina)
+      ordem = parseInt(ordem)
 
       if(limite > 0 && pagina > 0) {
         const listaLivros = await Livro.find({})
+        .sort({[campoOrdenacao]: ordem})
         .skip((pagina -1) * limite)
         .limit(limite)
           .populate('autor')
