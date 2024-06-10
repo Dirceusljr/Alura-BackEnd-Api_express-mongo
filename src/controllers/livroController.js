4538af39ef99bfcd60efd84d142691bf559c0553
Dirceusljr <dirceusljr@gmail.com>
2024-06-09 22:20:31 -0300

    implementando busca por termos

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -69,9 +69,14 @@ class LivroController {
 
   static async ListarLivrosPorFiltro (req, res, next) {
     const { editora, titulo } = req.query
+
+    //Solução com java para pesquisa de termos
+    // const regex = new RegExp(titulo, 'i')
+
     const busca = {};
     if (editora) busca.editora = editora
-    if (titulo) busca.titulo = titulo
+    // if (titulo) busca.titulo = regex
+    if (titulo) busca.titulo = {$regex: titulo, $options: 'i'}
 
     try {
       const livrosFiltrados = await Livro.find(busca)
